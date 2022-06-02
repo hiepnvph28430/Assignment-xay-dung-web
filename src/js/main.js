cfd48ba5c3aef83bc16e1bb3837221083be85c64
hiepnvph28430 <mrhiep165@gmail.com>
2022-06-02 14:01:28 +0700

    update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -29,12 +29,12 @@ function showProduct(products){
         const product = products[i];
         result += ` <div>
         <div class="product-img">
-            <a href="./detail.html?id=${product.id}">
+            <a href="detail.html?id=${product.id}">
                 <img src="${product.img}" alt="" />
             </a>
         </div>
         <div class="product-content">
-            <h3><a href="./detail.html?id=${product.id}">${product.name}</a></h3>
+            <h3><a href="detail.html?id=${product.id}">${product.name}</a></h3>
             <div class="flex">
                 <span>${product.time} times</span>
                 <span>${product.type}</span>
