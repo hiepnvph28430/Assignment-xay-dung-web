d4ce2b06ad7786c2ae6958deb3097f8174fb434b
hiepnvph28430 <mrhiep165@gmail.com>
2022-06-03 00:19:09 +0700

    feat:detail

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,4 +1,45 @@
-const productList = [  // mảng
+// const productList = [  // mảng
+//     {
+//         id: 1,
+//         name: "Product A",
+//         img: "https://picsum.photos/300/300",
+//         time: 30,
+//         type: "snack",
+//     },
+//     {
+//         id: 2,
+//         name: "Product B",
+//         img: "https://picsum.photos/300/300",
+//         time: 60,
+//         type: "snack",
+//     },
+//     {
+//         id: 3,
+//         name: "Product C",
+//         img: "https://picsum.photos/300/300",   // object
+//         time: 10,
+//         type: "fish",
+//     },
+// ];
+// // lấy ID trên url
+// const id =  new URLSearchParams(window.location.search).get("id");
+
+// function showProduct(products){
+//     // nếu mảng không phải 1 mảng hoặc không có giá trị trong mảng thì sẽ return false
+//     if(!Array.isArray(products) || products.length == 0 )  return false;
+
+//     let result = "";
+//     // tìm phần tử trong mảng, nếu id trên url = id phần tử trong mảng
+//     // thì trả về object đấy
+
+//     const product = products.find((item) => item.id == id);
+//     // trả về kết quả là 1 chuỗi 
+//     result = `<h2>${product.name}</h2>`
+//     return result;
+// }
+
+// document.getElementById("detail").innerHTML = showProduct(productList);
+const productList = [
     {
         id: 1,
         name: "Product A",
@@ -16,25 +57,25 @@ const productList = [  // mảng
     {
         id: 3,
         name: "Product C",
-        img: "https://picsum.photos/300/300",   // object
+        img: "https://picsum.photos/300/300",
         time: 10,
         type: "fish",
     },
 ];
-// lấy ID trên url
-const id =  new URLSearchParams(window.location.search).get("id");
+// Lấy ID trên url
+const id = new URLSearchParams(window.location.search).get("id");
 
-function showProduct(products){
-    // nếu mảng không phải 1 mảng hoặc không có giá trị trong mảng thì sẽ return false
-    if(!Array.isArray(products) || products.length == 0 )  return false;
+function showProduct(products) {
+    // Nếu không phải 1 mảng hoặc không có giá trị trong mảng thì return false
+    if (!Array.isArray(products) || products.length == 0) return false;
 
     let result = "";
-    // tìm phần tử trong mảng, nếu id trên url = id phần tử trong mảng
+    // Tìm phần tử trong mảng, nếu id trên url == id phần tử trong mảng
     // thì trả về object đấy
-
     const product = products.find((item) => item.id == id);
-    // trả về kết quả là 1 chuỗi 
-    result = `<h2>${product.name}</h2>`
+
+    // trả về kết quả là 1 chuỗi
+    result = `<h2>${product.name}</h2>`;
     return result;
 }
 
